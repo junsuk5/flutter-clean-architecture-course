370e94d77a1a610c7736fa15833ccbf866d5368c
Junsuk Oh <a811219@gmail.com>
2021-11-27 23:56:25 +0900

    노트 앱 - DI 설정

---
+++ note_app/lib/di/provider_setup.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_note_app/data/data_source/note_db_helper.dart';
+import 'package:flutter_note_app/data/repository/note_repository_impl.dart';
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
+import 'package:flutter_note_app/presentation/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<List<SingleChildWidget>> getProviders() async {
+  Database database = await openDatabase(
+    'notes_db',
+    version: 1,
+    onCreate: (db, version) async {
+      await db.execute(
+          'CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)');
+    },
+  );
+
+  NoteDbHelper noteDbHelper = NoteDbHelper(database);
+  NoteRepository repository = NoteRepositoryImpl(noteDbHelper);
+  NotesViewModel notesViewModel = NotesViewModel(repository);
+  AddEditNoteViewModel addEditNoteViewModel = AddEditNoteViewModel(repository);
+
+  return [
+    ChangeNotifierProvider(create: (_) => notesViewModel),
+    ChangeNotifierProvider(create: (_) => addEditNoteViewModel),
+  ];
+}
--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/di/provider_setup.dart';
 import 'package:flutter_note_app/presentation/notes/notes_screen.dart';
 import 'package:flutter_note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // 플랫폼 채널의 위젯 바인딩을 보장
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final providers = await getProviders();
+
+  runApp(
+    MultiProvider(
+      providers: providers,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {

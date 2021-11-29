ff5cd68b1703c8550a69b3854336da35f41e6237
Junsuk Oh <a811219@gmail.com>
2021-11-29 22:16:42 +0900

    노트 앱 - Mockito를 활용한 Test 코드 작성

---
+++ note_app/test/domain/use_case/get_notes_use_case_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_note_app/domain/use_case/get_notes_use_case.dart';
+import 'package:flutter_note_app/domain/util/note_order.dart';
+import 'package:flutter_note_app/domain/util/order_type.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'get_notes_use_case_test.mocks.dart';
+
+@GenerateMocks([NoteRepository])
+void main() {
+  test('정렬 기능이 잘 동작해야 한다', () async {
+    final repository = MockNoteRepository();
+    final getNotes = GetNotesUseCase(repository);
+
+    // 동작 정의
+    when(repository.getNotes()).thenAnswer((_) async => [
+      Note(title: 'title', content: 'content', timestamp: 0, color: 1),
+      Note(title: 'title2', content: 'content2', timestamp: 2, color: 2),
+    ]);
+
+    List<Note> result = await getNotes(const NoteOrder.date(OrderType.descending()));
+    expect(result, isA<List<Note>>());
+    expect(result.first.timestamp, 2);
+    verify(repository.getNotes());
+
+    result = await getNotes(const NoteOrder.date(OrderType.ascending()));
+    expect(result.first.timestamp, 0);
+    verify(repository.getNotes());
+
+    result = await getNotes(const NoteOrder.title(OrderType.ascending()));
+    expect(result.first.title, 'title');
+    verify(repository.getNotes());
+
+    result = await getNotes(const NoteOrder.title(OrderType.descending()));
+    expect(result.first.title, 'title2');
+    verify(repository.getNotes());
+
+    result = await getNotes(const NoteOrder.color(OrderType.ascending()));
+    expect(result.first.color, 1);
+    verify(repository.getNotes());
+
+    result = await getNotes(const NoteOrder.color(OrderType.descending()));
+    expect(result.first.color, 2);
+    verify(repository.getNotes());
+
+    verifyNoMoreInteractions(repository);
+  });
+}

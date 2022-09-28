cbce5ffe523f2aa733eeb17da6712918a54c8b45
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-09-29 00:57:00 +0900

    GetIt 으로 의존성 주입 코드 개선

--- note_app/lib/di/provider_setup.dart
+++ note_app/lib/di/provider_setup.dart
@@ -9,48 +9,40 @@ import 'package:flutter_note_app/domain/use_case/update_note_use_case.dart';
 import 'package:flutter_note_app/domain/use_case/use_cases.dart';
 import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
 import 'package:flutter_note_app/presentation/notes/notes_view_model.dart';
+import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sqflite/sqflite.dart';
 
 final getIt = GetIt.instance;
 
 Future setupDi() async {
-  Database database = await openDatabase(
-    'notes_db',
-    version: 1,
-    onCreate: (db, version) async {
-      await db.execute(
-          'CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)');
-    },
-  );
-
-  getIt.registerSingleton<Database>(database);
-  getIt.registerSingleton<NoteDbHelper>(NoteDbHelper(getIt.get<Database>()));
-  getIt.registerSingleton<NoteRepository>(
-      NoteRepositoryImpl(getIt.get<NoteDbHelper>()));
-  getIt.registerSingleton<AddNoteUseCase>(
-      AddNoteUseCase(getIt.get<NoteRepository>()));
-  getIt.registerSingleton<DeleteNoteUseCase>(
-      DeleteNoteUseCase(getIt.get<NoteRepository>()));
-  getIt.registerSingleton<GetNoteUseCase>(
-      GetNoteUseCase(getIt.get<NoteRepository>()));
-  getIt.registerSingleton<GetNotesUseCase>(
-      GetNotesUseCase(getIt.get<NoteRepository>()));
-  getIt.registerSingleton<UpdateNoteUseCase>(
-      UpdateNoteUseCase(getIt.get<NoteRepository>()));
+  getIt.registerSingletonAsync(() => openDatabase(
+        'notes_db',
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute(
+              'CREATE TABLE note (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)');
+        },
+      ));
+  getIt.registerSingleton(NoteDbHelper(await getIt.getAsync()));
+  getIt.registerSingleton<NoteRepository>(NoteRepositoryImpl(getIt.get()));
+  getIt.registerSingleton(AddNoteUseCase(getIt.get()));
+  getIt.registerSingleton(DeleteNoteUseCase(getIt.get()));
+  getIt.registerSingleton(GetNoteUseCase(getIt.get()));
+  getIt.registerSingleton(GetNotesUseCase(getIt.get()));
+  getIt.registerSingleton(UpdateNoteUseCase(getIt.get()));
 
   getIt.registerFactory(
     () => NotesViewModel(
       UseCases(
-        addNote: getIt.get<AddNoteUseCase>(),
-        deleteNote: getIt.get<DeleteNoteUseCase>(),
-        getNote: getIt.get<GetNoteUseCase>(),
-        getNotes: getIt.get<GetNotesUseCase>(),
-        updateNote: getIt.get<UpdateNoteUseCase>(),
+        addNote: getIt.get(),
+        deleteNote: getIt.get(),
+        getNote: getIt.get(),
+        getNotes: getIt.get(),
+        updateNote: getIt.get(),
       ),
     ),
   );
 
-  getIt
-      .registerFactory(() => AddEditNoteViewModel(getIt.get<NoteRepository>()));
+  getIt.registerFactory(() => AddEditNoteViewModel(getIt.get()));
 }
